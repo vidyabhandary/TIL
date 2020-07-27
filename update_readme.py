7ab7093d4e88250e7708f788715b68844451a56f
Vidya <vidya.ga@gmail.com>
2020-07-27 21:41:22 +0530

    Add Count of TILs

--- update_readme.py
+++ update_readme.py
@@ -7,6 +7,9 @@ import re
 root = pathlib.Path(__file__).parent.resolve()
 
 index_re = re.compile(r"<!\-\- index starts \-\->.*<!\-\- index ends \-\->", re.DOTALL)
+count_re = re.compile(r"<!\-\- count starts \-\->.*<!\-\- count ends \-\->", re.DOTALL)
+
+COUNT_TEMPLATE = "<!-- count starts -->{}<!-- count ends -->"
 
 if __name__ == "__main__":
     db = sqlite_utils.Database(root / "til.db")
@@ -30,6 +33,8 @@ if __name__ == "__main__":
         readme = root / "README.md"
         index_txt = "\n".join(index).strip()
         readme_contents = readme.open().read()
-        readme.open("w").write(index_re.sub(index_txt, readme_contents))
+        rewritten = index_re.sub(index_txt, readme_contents)
+        rewritten = count_re.sub(COUNT_TEMPLATE.format(db["til"].count), rewritten)
+        readme.open("w").write(rewritten)    
     else:
         print("\n".join(index))
